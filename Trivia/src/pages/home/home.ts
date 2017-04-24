243a73fca1210669ad88604931a9f986c3c3d0a5
Pablo De Cecco <pablomdececcorios@gmail.com>
2017-04-24 09:49:07 -0300

    inicio sesion

--- Trivia/src/pages/home/home.ts
+++ Trivia/src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @Input() email;
+  @Input() password;
+  name:string; 
   constructor(public navCtrl: NavController) {
-
+    this.name='nombre';
+    console.log(this.name);
   }
-
+  
 }
