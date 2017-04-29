776670c68e7bda4faef57bec9ac0ebbea80c98f2
alumno <alumno>
2017-04-29 10:12:22 -0300

    arreglo de home

--- Trivia/src/pages/home/home.ts
+++ Trivia/src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component ,Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';//FORMBUILDER CREA FORMS, FORMGROUP DEFINE UN FORMULARIO Y VALIDATORS CONTIENE VALIDACIONES PREDISEÑADAS
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,12 +7,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //jugadas : Jugada[];
+  miForm : FormGroup;
+  errorEnFormulario: boolean
   @Input() email;
   @Input() password;
   name:string; 
-  constructor(public navCtrl: NavController) {
-    this.name='nombre';
-    console.log(this.name);
+  constructor(public navCtrl: NavController,public formBuilder: FormBuilder) {
+     this.miForm = formBuilder.group({
+        nombre: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+    });
+    
   }
   
 }
