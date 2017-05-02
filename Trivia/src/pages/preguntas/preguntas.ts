799905a2c9e20b3d4a9cdede88ed384b2a157ee7
Pablo De Cecco <pablomdececcorios@gmail.com>
2017-05-02 17:27:03 -0300

    Creacion de preguntas v3

--- Trivia/src/pages/preguntas/preguntas.ts
+++ Trivia/src/pages/preguntas/preguntas.ts
@@ -17,15 +17,31 @@ export class Preguntas {
   pregunta:string;
   respuestas:{};
   rFinal:string[];
+  imagen:string;
+  bandera:boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.pregunta='¿En que año se estrenó volver al futuro?';
     this.respuestas=[{value:'1983',correcto:false},{value:'1985',correcto:true},{value:'1990',correcto:false}];
+    this.imagen='http://localhost/UTN/PrimerParcialPPS2017-1Cuatri/Trivia/delorean.jpg';
      
   }
-  seleccion(E){        
-    this.pregunta='¿Cuál de estos actores NO participo en la pelicula TITANIC?';
-    this.respuestas=[{value:'Leonardo DiCaprio',correcto:false},{value:'Steven Seagal',correcto:true},{value:'Kate Winslet',correcto:false}];
-    
+  
+  seleccion(E){    
+    if(this.bandera){
+     setTimeout(()=>{
+      this.pregunta='¿Cuál de estos actores NO participo en la pelicula TITANIC?';    
+      this.respuestas=[{value:'Steven Seagal',correcto:true},{value:'Steven Seagal',correcto:false},{value:'Kate Winslet',correcto:false}];
+      this.imagen='http://localhost/UTN/PrimerParcialPPS2017-1Cuatri/Trivia/titanic.jpg';
+    }, 200);
+  }else
+  {
+    this.bandera=true;
+    setTimeout(()=>{
+      this.pregunta='¿Qué pelicula parodian los simpsons en esta escena?';    
+      this.respuestas=[{value:'El eterno resplandor de una mente sin recuerdos',correcto:false},{value:'El resplandor',correcto:true},{value:'Resplandeciente',correcto:false}];
+      this.imagen='http://localhost/UTN/PrimerParcialPPS2017-1Cuatri/Trivia/simpsons.jpg';
+    }, 200);
+  }
   };
   ionViewDidLoad() {
     console.log('ionViewDidLoad Preguntas');
